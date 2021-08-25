238fcbe513360cc019fd3de9a9e7d15fbc7b0ddc
d.zharikhin <d.zharikhin@hh.ru>
2021-08-25 23:11:33 +0300

    use rw locks instead atomicboolean

--- src/main/java/Balancer.java
+++ src/main/java/Balancer.java
@@ -9,39 +9,43 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Balancer {
 
-  private static final AtomicBoolean statsFrozen = new AtomicBoolean();
+  private static final ReadWriteLock lock = new ReentrantReadWriteLock();
+  private static final Lock readLock = lock.readLock();
+  private static final Lock writeLock = lock.writeLock();
 
   public static void main(String[] args) throws InterruptedException {
     Clock clock = Clock.systemDefaultZone();
     Instant start = clock.instant();
     ConcurrentHashMap<Integer, List<Integer>> counter = new ConcurrentHashMap<>();
-    List<Server> servers = List.of(new Server(0, 200), new Server(1, 50));
+    List<Server> servers = List.of(new Server(200), new Server(50));
     ExecutorService executorService = Executors.newFixedThreadPool(16);
     executorService.invokeAll(IntStream.range(0, 10_000).mapToObj(ignored -> (Callable<?>) () -> {
       int minIndex = -1;
       Weight minWeight = null;
-      while (!statsFrozen.compareAndSet(false, true)) {
-        Thread.onSpinWait();
-      }
-      for (int i = 0; i < servers.size(); i++) {
-        Server server = servers.get(i);
-        Map.Entry<Float, Float> load = server.getLoad();
-        Weight weight = new Weight(load.getValue(), load.getKey());
-        if ((minIndex < 0 || minWeight.compareTo(weight) > 0)) {
-          minIndex = i;
-          minWeight = weight;
+      //here we need to freeze all servers stats - so we use write lock for read
+      writeLock.lock();
+      try {
+        for (int i = 0; i < servers.size(); i++) {
+          Server server = servers.get(i);
+          Map.Entry<Float, Float> load = server.getLoad();
+          Weight weight = new Weight(load.getValue(), load.getKey());
+          if ((minIndex < 0 || minWeight.compareTo(weight) > 0)) {
+            minIndex = i;
+            minWeight = weight;
+          }
         }
-      }
-      servers.get(minIndex).acquire(minIndex);
-      while (!statsFrozen.compareAndSet(true, false)) {
-        Thread.onSpinWait();
+        servers.get(minIndex).acquire();
+      } finally {
+        writeLock.unlock();
       }
       // actual work with server
       Thread.sleep(10);
@@ -57,12 +61,12 @@ public class Balancer {
 
   private static void release(int index, List<Server> servers) {
     servers.get(index).release();
-    while (!statsFrozen.compareAndSet(false, true)) {
-      Thread.onSpinWait();
-    }
-    boolean needRescale = servers.stream().allMatch(server -> server.getStatLoad() > 1);
-    while (!statsFrozen.compareAndSet(true, false)) {
-      Thread.onSpinWait();
+    boolean needRescale;
+    writeLock.lock();
+    try {
+      needRescale = servers.stream().allMatch(server -> server.getStatLoad() > 1);
+    } finally {
+      writeLock.unlock();
     }
     if (needRescale) {
       servers.forEach(Server::rescale);
@@ -71,12 +75,10 @@ public class Balancer {
 
 
   private static class Server {
-    private final int index;
     private volatile int weight;
     private AtomicInteger stats = new AtomicInteger();
 
-    public Server(int index, int weight) {
-      this.index = index;
+    public Server(int weight) {
       this.weight = weight;
     }
 
@@ -91,27 +93,32 @@ public class Balancer {
       return (float) extractStatRequests(stats.intValue()) / weight;
     }
 
-    void acquire(int index) {
-      if (this.index != index) {
-        while (statsFrozen.get()) {
-          Thread.onSpinWait();
-        }
+    void acquire() {
+      // we don't need atomicity by lock here - just locking on global stat manipulations
+      readLock.lock();
+      try {
+        stats.addAndGet(packValueToStatRequests(1) + 1);
+      } finally {
+        readLock.unlock();
       }
-      stats.addAndGet(packValueToStatRequests(1) + 1);
     }
 
     void rescale() {
-      while (statsFrozen.get()) {
-        Thread.onSpinWait();
+      readLock.lock();
+      try {
+        stats.updateAndGet(currentValue -> extractStatRequests(currentValue) >= weight ? currentValue - packValueToStatRequests(weight) : currentValue);
+      } finally {
+        readLock.unlock();
       }
-      stats.updateAndGet(currentValue -> extractStatRequests(currentValue) >= weight ? currentValue - packValueToStatRequests(weight) : currentValue);
     }
 
     void release() {
-      while (statsFrozen.get()) {
-        Thread.onSpinWait();
+      readLock.lock();
+      try {
+        stats.updateAndGet(i -> i > 0 ? i - 1 : i);
+      } finally {
+        readLock.unlock();
       }
-      stats.updateAndGet(i -> i > 0 ? i - 1 : i);
     }
 
     private int extractStatRequests(int value) {
